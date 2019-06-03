75d50719c86f79f68d598b5ad2cf6e90307229ad
NinaSleephead <kx1214@gmail.com>
2019-06-02 19:45:41 -0600

    CURD

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -7,9 +7,7 @@ import { Col, Row } from 'reactstrap';
 const Footer = props => (
   <div className="footer page-content">
     <Row>
-      <Col md="12">
-        <p>Your footer</p>
-      </Col>
+      <Col md="12">{/* <p>Your footer</p> */}</Col>
     </Row>
   </div>
 );
--- src/main/webapp/app/shared/reducers/course.ts
+++ src/main/webapp/app/shared/reducers/course.ts
@@ -4,6 +4,9 @@ import { SUCCESS } from 'app/shared/reducers/action-type.util';
 
 export const ACTION_TYPES = {
   GET_COURSES: 'course/GET_COURSES',
+  ADD_COURSE: 'course/ADD_COURSE',
+  UPDATE_COURSE: 'course/UPDATE_COURSE',
+  DELETE_COURSE: 'course/DELETE_COURSE',
   CLEAR_COURSES: 'course/CLEAR_COURSES'
 };
 
@@ -16,10 +19,47 @@ export type ApplicationCourseState = Readonly<typeof initialState>;
 export default (state: ApplicationCourseState = initialState, action): ApplicationCourseState => {
   switch (action.type) {
     case SUCCESS(ACTION_TYPES.GET_COURSES):
+      console.log('get ' + JSON.stringify(state));
+      console.log('get ' + JSON.stringify(action.payload.data));
       return {
         ...state,
         courses: action.payload.data
       };
+    case SUCCESS(ACTION_TYPES.ADD_COURSE):
+      return {
+        ...state,
+        courses: action.payload.data
+      };
+    case SUCCESS(ACTION_TYPES.UPDATE_COURSE):
+      return {
+        ...state,
+        courses: action.payload.data
+      };
+    case SUCCESS(ACTION_TYPES.DELETE_COURSE):
+      //let newerState = { ...state };
+      console.log('newerState ' + JSON.stringify(state));
+      const indexOfDelete;
+      state.courses.filter((course, index) => {
+        const url = action.payload.config.url;
+        let courseName = url.replace('/api/course/deleteCourse/', '');
+        console.log('action.course.courseName ' + courseName);
+        console.log('course.courseName ' + course.courseName);
+        if (course.courseName == courseName) {
+          console.log('find');
+          console.log('find index ' + index);
+          indexOfDelete = index;
+          return indexOfDelete;
+        }
+        return -1;
+      });
+      console.log('indexOfDelete ' + indexOfDelete);
+      state.courses.splice(indexOfDelete, 1);
+      console.log('newerState ' + JSON.stringify(state));
+      //return newerState;
+      return {
+        ...state
+      };
+
     case ACTION_TYPES.CLEAR_COURSES:
       let newState = { ...state };
       delete newState.courses;
@@ -35,6 +75,23 @@ export const getCourses = () => dispatch =>
   dispatch({
     type: ACTION_TYPES.GET_COURSES,
     payload: axios.get('api/course/findAllCourses')
+  });
+
+export const addACourse = () => dispatch =>
+  dispatch({
+    type: ACTION_TYPES.ADD_COURSE,
+    payload: axios.post('/api/course/addCourse')
+  });
+export const updateACourse = () => dispatch =>
+  dispatch({
+    type: ACTION_TYPES.UPDATE_COURSE,
+    payload: axios.put('/api/course/updateCourse')
+  });
+
+export const deleteACourse = courseName => dispatch =>
+  dispatch({
+    type: ACTION_TYPES.DELETE_COURSE,
+    payload: axios.delete(`/api/course/deleteCourse/${courseName}`)
   });
 
 export const clearCourses = () => dispatch =>
